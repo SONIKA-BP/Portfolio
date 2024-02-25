d710ac0e397b9c94036ea60a84f28feb24f0420a
SONIKA-BP <158311073+SONIKA-BP@users.noreply.github.com>
2024-02-25 15:56:30 +0530

    Add files via upload

---
+++ portfolio/portfolio.js
@@ -0,0 +1,43 @@
+var tablinks=document.getElementsByClassName("tab-links");
+var tabcontents=document.getElementsByClassName("tab-contents");
+function openTab(tabname){
+    for(tablink of tablinks)
+    {
+        tablink.classList.remove("active-links")
+    }
+    for(tablecontent of tabcontents)
+    {
+        tablecontent.classList.remove("active-tab")
+    }
+    event.currentTarget.classList.add("active-links");
+    document.getElementById(tabname).classList.add("active-tab");
+}
+var sidemenu=document.getElementById("sidemenu");
+function openMenu()
+{
+ sidemenu.style.right="0";   
+}
+function closeMenu()
+{
+    
+        sidemenu.style.right="-200px";   
+    
+}
+
+  const scriptURL = 'https://script.google.com/macros/s/AKfycbzyjGQJx2l6Li8A5ebetS1EKBcN4plecG5LVKzDSOnoQcxFnKEibXbRpY8k7M0tDByb/exec'
+  const form = document.forms['submit-to-google-sheet']
+    const msg=document.getElementById("msg");
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response =>{
+        msg.innerHTML="Message sent successfully!!"
+        setTimeout(function()
+        {
+            msg.innerHTML=""
+        },5000)
+        form.reset()
+      } )
+      .catch(error => console.error('Error!', error.message))
+  })
+
